497a794d8c672ee78ae2d8b855702add9464be86
Victor Tran <vicr12345@gmail.com>
2023-11-11 00:50:08 +1100

    Add MeansImplicitUse

--- bridge/tdotnetbridge/tdotnetbridge.ClientLibrary/ExportToQtAttribute.cs
+++ bridge/tdotnetbridge/tdotnetbridge.ClientLibrary/ExportToQtAttribute.cs
@@ -1,6 +1,10 @@
+using JetBrains.Annotations;
+
 namespace tdotnetbridge.ClientLibrary;
 
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Property)]
+[MeansImplicitUse]
+[UsedImplicitly]
 public class ExportToQtAttribute : Attribute
 {
     
--- bridge/tdotnetbridge/tdotnetbridge.ClientLibrary/QObjectAttribute.cs
+++ bridge/tdotnetbridge/tdotnetbridge.ClientLibrary/QObjectAttribute.cs
@@ -1,6 +1,10 @@
-﻿namespace tdotnetbridge.ClientLibrary;
+﻿using JetBrains.Annotations;
+
+namespace tdotnetbridge.ClientLibrary;
 
 [AttributeUsage(AttributeTargets.Class)]
+[MeansImplicitUse]
+[UsedImplicitly]
 public class QObjectAttribute : Attribute
 {
 }
